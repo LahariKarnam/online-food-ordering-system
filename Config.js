67e421e277f70d05bd81605e54e8836086e2af41
LahariKarnam <121763378+LahariKarnam@users.noreply.github.com>
2025-04-08 09:26:38 +0530

    Create Config.js

---
+++ Config.js
@@ -0,0 +1,16 @@
+ window. config-(
+cognita: (
+userPlClientidep
+region.st-2
+Invokelüli //.. https://rc/myt4tal.execute-aps.us-west-2.amazonaws.com/prod",
+window._config = {
+cognito: {
+userPoolId: 'us-east-1_wspQvA7oV', 
+userPoolClientId: '7ip798quo72obck9ii7uiv75ph', 
+region: 'us-east-1'
+},
+api: {
+invokeUrl: // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+ }
+
+};
